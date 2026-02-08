14f924b3d0e0f361db7c370027531e0116f13d62
Alessiaa01 <occhionero.alessia01@gmail.com>
2026-02-08 08:38:54 +0100

    correzioni

--- src/main/java/it/uniroma3/siw/model/Ingrediente.java
+++ src/main/java/it/uniroma3/siw/model/Ingrediente.java
@@ -1,13 +1,16 @@
 package it.uniroma3.siw.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Ingrediente {
@@ -19,7 +22,8 @@ public class Ingrediente {
 	@Column(nullable = false, unique = true)
 	private String nome;
 	
-	
+	@OneToMany(mappedBy = "ingrediente", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<RicettaIngrediente> ricettaIngredienti = new ArrayList<>();
 	//costruttore vuoto
 	public Ingrediente () {
 		
